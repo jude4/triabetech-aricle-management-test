1a269879e815da56fb3ffcdddd29d1c7759f486e
Jude <judeufuomao@gmail.com>
2025-10-04 08:18:14 +0100

    fix

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -11,17 +11,18 @@ let db: PrismaClient;
 const isUsingAccelerate = process.env.DATABASE_URL?.startsWith(
   "@prisma+postgres://"
 );
+const isUsingSQLite = process.env.DATABASE_URL?.startsWith("file:");
 const isProduction = process.env.NODE_ENV === "production";
 
 // This is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 // In production we'll have a single connection to the DB.
-if (isUsingAccelerate || isProduction) {
+if (isUsingAccelerate) {
   // Use Prisma Accelerate for production
   db = new PrismaClient().$extends(withAccelerate());
 } else {
-  // Use standard Prisma client for local development
+  // Use standard Prisma client for local development (SQLite)
   if (!global.__db__) {
     global.__db__ = new PrismaClient();
   }
