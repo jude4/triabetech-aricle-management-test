67f74280cba97432a6c0e92b8505384e40a9d5a0
Jude <judeufuomao@gmail.com>
2025-10-04 07:10:45 +0100

    added prisma

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 declare global {
   var __db__: PrismaClient;
@@ -11,10 +12,10 @@ let db: PrismaClient;
 // create a new connection to the DB with every change either.
 // In production we'll have a single connection to the DB.
 if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
+  db = new PrismaClient().$extends(withAccelerate());
 } else {
   if (!global.__db__) {
-    global.__db__ = new PrismaClient();
+    global.__db__ = new PrismaClient().$extends(withAccelerate());
   }
   db = global.__db__;
   db.$connect();
