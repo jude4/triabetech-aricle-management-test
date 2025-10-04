14a37f063f1997f01a466c6327f632174a0657ce
Jude <judeufuomao@gmail.com>
2025-10-04 06:58:38 +0100

    added vercel to dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     remix({
       ignoredRouteFiles: ["**/.*"],
+      presets: [vercelPreset()],
+      future: {
+        v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
+        v3_relativeSplatPath: true,
+        v3_singleFetch: true,
+        v3_throwAbortReason: true,
+      },
     }),
   ],
 });
